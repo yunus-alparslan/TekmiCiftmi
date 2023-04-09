ff4406a24b7d3fb7b9d301a5757ef81668fd5311
Yunus ALPARSLAN <99866054+yunus-alparslan@users.noreply.github.com>
2023-04-09 20:34:25 +0300

    TekmiCiftmi

---
+++ SayıTekmiÇiftmi/SayıTekmiÇiftmi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayıTekmiÇiftmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayı = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayı = Convert.ToInt16(textBox1.Text);
+
+            if (sayı % 2 == 0)
+            {
+                label2.Text = "Çift";
+                this.BackColor = Color.AliceBlue;
+            }
+            else
+            {
+                label2.Text = "Tek";
+                this.BackColor = Color.Aqua;
+            }
+
+
+            
+
+
+        }
+    }
+}
